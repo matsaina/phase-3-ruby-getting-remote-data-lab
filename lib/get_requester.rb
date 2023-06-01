835d167d79456839cf64648bf4c8841b032a8840
matsaina <matsaina@gmail.com>
2023-06-01 13:32:54 +0300

    comlete

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,21 @@
-# Write your code here
+require 'net/http'
+require 'json'
+
+class GetRequester
+  attr_accessor :url
+
+  def initialize(url)
+    @url = url
+  end
+
+  def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    json_data = get_response_body
+    JSON.parse(json_data)
+  end
+end
